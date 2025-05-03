a78876e72816a7a1440a4ed29a8687bab0cb3d6c
harsshgpt <harshgupta1752003@gmail.com>
2025-05-03 14:37:01 +0530

    dsld

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let bt = document.querySelector("button");
+let h = document.querySelector("h1");
+
 bt.addEventListener("click",()=>{
-    fetch("https://api.github.com/users/harsshgpt").then(res=>res.json()).then((res)=>console.log(res
-        
-    ))
+    fetch("https://api.github.com/users/harsshgpt").then(res=>res.json()).then((res)=>{h.innerText = res.name })
 })
