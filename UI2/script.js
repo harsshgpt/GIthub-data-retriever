c67d97b9eee70748f898f664fb7a924c32da0351
harsshgpt <harshgupta1752003@gmail.com>
2025-05-04 17:52:23 +0530

    huh a

--- UI2/script.js
+++ UI2/script.js
@@ -1,9 +1,13 @@
 function getprofiledata (username){
-    fetch(`https://api.github.com/users/${username}`).then(raw => {
-        if(!raw.ok) alert("user not found");
+    return fetch(`https://api.github.com/users/${username}`).then((raw) => {
+        if(!raw.ok) throw new Error("User not found")
         return raw.json();
-    })
+    });
 
 }
-let a = getprofiledata("harsshgpt");
-console.log(a)
+function getrepo(username){
+  return fetch(`https://api.github.com/users/${username}/repos?sort=updated`).then(raw => raw.json())
+}
+ getrepo("harsshgpt").then(function(data){
+    console.log(data)
+ })
