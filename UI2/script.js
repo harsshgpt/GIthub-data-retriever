2758ce688da35881c70550a20c24156396178110
harsshgpt <harshgupta1752003@gmail.com>
2025-05-04 18:38:29 +0530

    nn,

--- UI2/script.js
+++ UI2/script.js
@@ -1,13 +1,31 @@
+let btn = document.getElementById("submitBtn")
+let input = document.getElementById("usernameInput")
+let img = document.getElementById("avatar")
+let name = document.getElementById("name");
+let username = document.getElementById("username")
+const bio = document.getElementById("bio")
+const repos = document.getElementById("repos");
+const followers = document.getElementById("followers");
+const following = document.getElementById("following");
+const locationEl = document.getElementById("location");
+
 function getprofiledata (username){
     return fetch(`https://api.github.com/users/${username}`).then((raw) => {
         if(!raw.ok) throw new Error("User not found")
         return raw.json();
     });
+}
 
-}
-function getrepo(username){
-  return fetch(`https://api.github.com/users/${username}/repos?sort=updated`).then(raw => raw.json())
-}
- getrepo("harsshgpt").then(function(data){
-    console.log(data)
- })
+btn.addEventListener("click",function(){
+    const user = input.value.trim();
+    let details  = 0
+    getprofiledata(user).then(function(data){
+       img.src =  data.avatar_url;
+    })
+    
+    
+})
+
+
+
+
